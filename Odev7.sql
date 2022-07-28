70618a4cde39d7952c727cab0bccf82dc03169f7
istarhan <51276149+istarhan@users.noreply.github.com>
2022-07-28 18:55:49 +0300

    Update Odev7.sql

--- Odev7.sql
+++ Odev7.sql
@@ -1,1 +1,8 @@
 --Soru 1
+SELECT rating FROM film GROUP BY rating;
+--Soru 2
+SELECT replacement_cost, COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*)>50;
+--Soru 3
+SELECT store_id, COUNT(*) FROM customer GROUP BY store_id;
+--Soru 4
+SELECT country_id, COUNT(city) FROM city GROUP BY country_id ORDER BY COUNT(city) DESC LIMIT 1;
